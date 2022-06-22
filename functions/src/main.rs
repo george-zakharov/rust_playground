b669d2b4736191cce2833485f4fa805c220204e1
George Zakharov <george_zakharov@mail.ru>
2022-06-23 01:02:54 +0300

    String check

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,15 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    and_another_one();
 }
 
 fn another_function() {
     println!("Another function!");
+}
+
+fn and_another_one() {
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
 }
