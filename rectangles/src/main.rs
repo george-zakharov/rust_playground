2c61f6ac541e31fce48dce608a6cb3da3ac2213b
George Zakharov <george_zakharov@mail.ru>
2023-01-09 21:33:11 +0400

    Rectangle struct check

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,15 +1,28 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let _width1 = 30;
     let _height1 = 50;
-    let rect1 = (30, 50);
+    let _rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of rectangle is {} square pixels.",
-        area2(rect1)
+        area3(&rect1)
     );
 }
 
-fn area2(dimensions: (u32, u32)) -> u32 {
+fn area3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn _area2(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
 }
 
